581a6d06159ce35a533eaaae6893df6f6f762360
jthegedus <jthegedus@hey.com>
2025-09-29 10:28:41 +1000

    feat: source homebrew on macos & linux

--- .config/bash/conf.d/21-homebrew.bash
+++ .config/bash/conf.d/21-homebrew.bash
@@ -1,4 +1,23 @@
-# Homebrew configuration for Linux
-if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
+# Homebrew configuration for Linux and macOS
+case "$(uname)" in
+    Darwin)
+        # macOS - check common installation paths
+        if [[ -x /opt/homebrew/bin/brew ]]; then
+            eval "$(/opt/homebrew/bin/brew shellenv)"
+        elif [[ -x /usr/local/bin/brew ]]; then
+            eval "$(/usr/local/bin/brew shellenv)"
+        else
+            echo "Homebrew not found on $(uname)" >&2
+        fi
+        ;;
+    Linux)
+        # Linux - check system and user installation paths
+        if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+            eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        elif [[ -x ~/.linuxbrew/bin/brew ]]; then
+            eval "$(~/.linuxbrew/bin/brew shellenv)"
+        else
+            echo "Homebrew not found on $(uname)" >&2
+        fi
+        ;;
+esac
--- .config/fish/conf.d/21-homebrew.fish
+++ .config/fish/conf.d/21-homebrew.fish
@@ -1,4 +1,23 @@
-# Homebrew configuration for Linux
-if test -x /home/linuxbrew/.linuxbrew/bin/brew
-    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# Homebrew configuration for Linux and macOS
+set --local homebrew_found false
+
+switch (uname)
+    case Darwin
+        # macOS - check common installation paths
+        if test -x /opt/homebrew/bin/brew
+            eval (/opt/homebrew/bin/brew shellenv)
+        else if test -x /usr/local/bin/brew
+            eval (/usr/local/bin/brew shellenv)
+        else
+            echo "Homebrew not found on $(uname)" >&2
+        end
+    case Linux
+        # Linux - check system and user installation paths
+        if test -x /home/linuxbrew/.linuxbrew/bin/brew
+            eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+        else if test -x ~/.linuxbrew/bin/brew
+            eval (~/.linuxbrew/bin/brew shellenv)
+        else
+            echo "Homebrew not found on $(uname)" >&2
+        end
 end
