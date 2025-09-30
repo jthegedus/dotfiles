e044a04c755f6fc82c8721a711d8137b673c8064
jthegedus <jthegedus@hey.com>
2025-10-01 02:20:58 +1000

    fix: color of shell text in prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt --description 'Simple prompt'
-    printf '%s%s@%s %s%s %s%s%s\n$ ' \
+    printf '%s%s@%s %s%s%s %s%s%s\n$ ' \
         (set_color brblue) $USER $hostname \
-        (set_color brwhite) (basename $SHELL) \
+        (set_color brcyan) (basename $SHELL) (set_color normal) \
         (set_color $fish_color_cwd) $PWD (set_color normal)
 end
