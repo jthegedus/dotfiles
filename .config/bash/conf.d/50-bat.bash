6693cda4ea06c5d6ac7249035c80cbbca00020aa
jthegedus <jthegedus@hey.com>
2025-09-30 15:03:32 +1000

    feat: alias cat to bat

---
+++ .config/bash/conf.d/50-bat.bash
@@ -0,0 +1,4 @@
+# Bat - cat clone with syntax highlighting
+if command -v bat &> /dev/null; then
+    alias cat='bat'
+fi
---
+++ .config/fish/conf.d/50-bat.fish
@@ -0,0 +1,4 @@
+# Bat - cat clone with syntax highlighting
+if command --query bat
+    alias cat 'bat'
+end
