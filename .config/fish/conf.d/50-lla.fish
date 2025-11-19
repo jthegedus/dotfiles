07907ce7d318b43aec169d76147d9a28ba9f0ab6
jthegedus <jthegedus@hey.com>
2025-11-19 18:36:01 +1100

    chore: alias for lla --tree as tree command

--- .config/fish/conf.d/50-lla.fish
+++ .config/fish/conf.d/50-lla.fish
@@ -16,6 +16,7 @@ if command --query lla
     alias lg 'lla -G'
     alias lt 'lla --timeline'
     alias lsize 'lla --sizemap --include-dirs'
+    alias tree 'lla --tree'
 
     # Note: The 'j' jump function is manually managed in .config/fish/functions/j.fish
     # We do not use 'lla jump --setup' to avoid automatic injection into config.fish
