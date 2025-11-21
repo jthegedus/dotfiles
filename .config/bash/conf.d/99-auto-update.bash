8c3d3aea9b0f3fb3c765cf9bb4925b24b6c75d86
jthegedus <jthegedus@hey.com>
2025-11-21 14:07:33 +1100

    fix: ignore auto-update when VSCode-based IDEs are starting a shell

--- .config/bash/conf.d/99-auto-update.bash
+++ .config/bash/conf.d/99-auto-update.bash
@@ -1,5 +1,5 @@
 # auto-update dotfiles repository and homebrew (once per day)
-if [[ $- == *i* ]]; then
+if [[ $- == *i* ]] && [[ -z "${VSCODE_RESOLVING_ENVIRONMENT}" ]]; then
     [[ "$ENABLE_SSH_CHECK" == "1" ]] && ssh-check
     [[ "$ENABLE_DOTFILES_UPDATE" == "1" ]] && update_dotfiles_repository
     [[ "$ENABLE_BREW_UPDATE" == "1" ]] && update_brew
--- .config/fish/conf.d/99-auto-update.fish
+++ .config/fish/conf.d/99-auto-update.fish
@@ -1,6 +1,6 @@
 # auto-update dotfiles repository and homebrew (once per day)
-if status --is-interactive
-    test "$ENABLE_SSH_CHECK" = "1"; and ssh-check
-    test "$ENABLE_DOTFILES_UPDATE" = "1"; and update_dotfiles_repository
-    test "$ENABLE_BREW_UPDATE" = "1"; and update_brew
+if status --is-interactive; and not set -q VSCODE_RESOLVING_ENVIRONMENT
+    test "$ENABLE_SSH_CHECK" = 1; and ssh-check
+    test "$ENABLE_DOTFILES_UPDATE" = 1; and update_dotfiles_repository
+    test "$ENABLE_BREW_UPDATE" = 1; and update_brew
 end
