f02cbcba61e9cc6a7af0295b933a3518f7b96c99
jthegedus <jthegedus@hey.com>
2026-01-18 23:56:09 +1100

    feat: add fonts to brew, oksh & improve system update function

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,23 +1,37 @@
-function update --description "Update system packages via Homebrew"
-    if command -v brew
-        echo "--- brew update ---"
-        brew update
-        echo "--- brew upgrade --greedy ---"
-        brew upgrade --greedy
-    end
-
-    if command -v pacman
-        echo "--- pacman upgrade ---"
-        sudo pacman -Syu
-    end
-
+function update --description "Update: firmware, system, userland cli & apps"
+    # Firmware
     if command -v fwupdmgr
+        echo
         echo "--- fwupd firmware update ---"
         fwupdmgr refresh
         fwupdmgr update
     end
 
+    # System
+    if command -v pacman
+        echo
+        echo "--- pacman upgrade ---"
+        sudo pacman -Syu
+    end
+
+    # Userland CLI & Apps
+    if command -v brew
+        echo
+        echo "--- brew update ---"
+        brew update
+
+        echo
+        echo "--- brew bundle ---"
+        brew bundle --file=$HOME/.config/brewfile/Brewfile
+
+        echo
+        echo "--- brew upgrade --greedy ---"
+        brew upgrade --greedy
+        echo
+    end
+
     if command -v flatpak
+        echo
         echo "--- flatpak update ---"
         flatpak update
     end
